8856881797a7f2deb12378f7b2251a4753b41e57
hhertout <hugues.hertout@gmail.com>
2025-12-06 18:04:09 +0100

    fix: lint

--- probe/src/http/tests/mod.rs
+++ probe/src/http/tests/mod.rs
@@ -1,8 +1,4 @@
-//! HTTP Probe Tests
-//!
-//! This module contains all unit and integration tests for the HTTP probe.
-
-mod metrics_tests;
-mod resolver_tests;
 mod client_tests;
 mod integration_tests;
+mod metrics_tests;
+mod resolver_tests;
